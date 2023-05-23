5aa59226938183e2e923fbaae6a1158d572ca898
SergiyZatsarinniy <Zatsarinniys@gmail.com>
2023-05-23 18:11:24 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+let formData = {};
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+refs.input.addEventListener('input', e => {
+  formData[e.target.name] = e.target.value;
+});
+
+populateFeedbackForm();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log('Form submitted!');
+  console.log('Email:', formData.email);
+  console.log('Message:', formData.message);
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateFeedbackForm() {
+  const data = localStorage.getItem(STORAGE_KEY);
+  if (data) {
+    formData = JSON.parse(data);
+
+    refs.input.value = formData.email;
+    refs.textarea.value = formData.message;
+  }
+}
